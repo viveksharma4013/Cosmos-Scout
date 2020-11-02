c8bd6c16e5525a2a1a9bd5938a734076ed46c81f
unknown <viveksharma4013@gmail.com>
2020-11-02 15:02:53 +0530

    check

--- src/components/pictureofday/InfoDisplay.js
+++ src/components/pictureofday/InfoDisplay.js
@@ -5,7 +5,7 @@ export default function InfoDisplay({data}) {
         <div className="newsDisplay">
         <div className="textdisplay">
             <img id="image" alt="Space" src="https://apod.nasa.gov/apod/image/2011/Trifid_HubbleGendler_2400.jpg"></img>
-            <p className="explanation">{data}</p>
+            <p className="explanation">{data}{data}</p>
         </div>
         </div>
     )
